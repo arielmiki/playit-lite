ccc495ad505b3d118f92f172bfe9dbc5e44bc7e4
arielmiki <arielabraham82@gmail.com>
2020-04-18 22:30:10 +0700

    Finish server implementation

--- server.py
+++ server.py
@@ -1,14 +1,28 @@
 import socket
 from model import MouseEvent
+from pynput.mouse import Button, Controller
+import pyautogui
 
 class MouseListenerServer:
     def __init__(self, host = 'localhost', port = 5050):
         self.port = port
         self.host = host
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.mouse = Controller()
+        self.width, self.height = pyautogui.size()
     
     def __process_data(self, data):
-        print(MouseEvent.decode(data))
+        e = MouseEvent.decode(data)
+        if e.type == MouseEvent.Type.ON_MOVE:
+            self.mouse.position = (e.x * self.width, e.y * self.height)
+        elif e.type == MouseEvent.Type.ON_CLICK:
+            if e.pressed:
+                self.mouse.press(e.button)
+            else:
+                self.mouse.release(e.button)
+        elif e.type == MouseEvent.Type.ON_SCROLL:
+            print(e.dx, e.dy)
+            self.mouse.scroll(e.dx, e.dy)
 
     def start(self):
         self.socket.bind((self.host, self.port))
@@ -18,6 +32,6 @@ class MouseListenerServer:
 
 
 if __name__ == '__main__':
-    client = MouseListenerServer()
+    client = MouseListenerServer("0.0.0.0", 20071)
     client.start() 
 
