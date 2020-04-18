ebd3c1cd8cded36b012bfa91dd99b712299400b0
arielmiki <arielabraham82@gmail.com>
2020-04-18 22:30:20 +0700

    Finish client implementation

--- client.py
+++ client.py
@@ -1,11 +1,13 @@
 from pynput import mouse
 from model import MouseEvent
+import pyautogui
 import socket
 
 class MouseListenerClient:
     def __init__(self, host: str = 'localhost', port: int = 5050):
         self.port = port
         self.host = host
+        self.width, self.height = pyautogui.size()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
     def start(self):
@@ -13,19 +15,20 @@ class MouseListenerClient:
             listener.join()
 
     def __on_click(self, x, y, button, pressed):
-        e = MouseEvent(MouseEvent.Type.ON_CLICK, x, y, button=button, pressed=pressed)
+        e = MouseEvent(MouseEvent.Type.ON_CLICK, x/self.width, y/self.height, button=button, pressed=pressed)
         self.socket.sendto(MouseEvent.encode(e), (self.host, self.port))
         
     def __on_move(self, x, y):
-        e = MouseEvent(MouseEvent.Type.ON_MOVE, x, y)
+        e = MouseEvent(MouseEvent.Type.ON_MOVE, x/self.width, y/self.height)
         self.socket.sendto(MouseEvent.encode(e), (self.host, self.port))
 
     def __on_scroll(self, x, y, dx, dy):
-        e = MouseEvent(MouseEvent.Type.ON_MOVE, x, y, dx=dx, dy=dy)
+        e = MouseEvent(MouseEvent.Type.ON_SCROLL, x/self.width, y/self.height, dx=dx, dy=dy)
+        print(dx, dy)
         self.socket.sendto(MouseEvent.encode(e), (self.host, self.port))
 
 
 
 if __name__ == '__main__':
-    client = MouseListenerClient()
+    client = MouseListenerClient(host = "192.168.100.7", port = 20071)
     client.start()    
